5d87ee4f069a1e850f3197763db7b20858a6b2d2
francismaria <franciscomaria97@hotmail.com>
2017-11-03 11:26:17 +0000

    Updated text file parser, parses ok but the date

--- src/main.cpp
+++ src/main.cpp
@@ -10,36 +10,89 @@
 #include <fstream>
 
 #include "Interface.h"
-//Include Interface [{while user !pressed exit... }
 
-void importClients(){
+void importClients(MovingCompany& company){
 	std::ifstream clientsFile;
-	clientsFile.open("../InfoFiles/clients.txt");
+	clientsFile.open("InfoFiles/clients.txt");
+
+	if(!clientsFile.good())
+		std::cout << "Couldn't access clients.txt file. Error reading from File." << std::endl;
 
 	int fDay, fMonth, fYear, age;
-	long int NIF;
-	std::string name, zipCode, address, line;
+	long int nif;
+	std::string name, zipCode, address, line, city, country;
 
 	while(std::getline(clientsFile, line)){
-		std::string::size_type i = 0;
 
-		i = line.find("/");
-		name = line.substr(0, i);
-		line = line.substr(i + 1);
+		std::stringstream ss(line);
+		std::string aux;
+		std::stringstream auxSS;
 
-		i = line.find("/");
-		age = std::stoi(line.substr(0, i));
-		line = line.substr(i + 1);
+		// Name
+		std::getline(ss, name, '/');
+
+		// Age
+		std::getline(ss, aux, '/');
+		auxSS << aux;
+		auxSS >> age;
+
+		aux.clear();
+		auxSS.str(std::string());
+
+		// NIF
+		std::getline(ss, aux, '/');
+		auxSS << aux;
+		auxSS >> nif;
+
+		aux.clear();
+		auxSS.str(std::string());
+
+		// Address
+		std::getline(ss, address, '/');
+
+		// Zip Code
+		std::getline(ss, zipCode, '/');
+
+		// City
+		std::getline(ss, city, '/');
+
+		// Country
+		std::getline(ss, country, '/');
+/*
+		// Data
+		std::getline(ss, aux, '/');
+		auxSS << aux;				//Day
+		auxSS >> fDay;
+
+		aux.clear();
+		auxSS.str(std::string());
+
+		std::getline(ss, aux, '/');
+		auxSS << aux;				//Month
+		auxSS >> fMonth;
+
+		aux.clear();
+		auxSS.str(std::string());
 
 
+		std::getline(ss, aux, '/');
+		auxSS << aux;				//Year
+		auxSS >> fYear;
 
+		aux.clear();
+		auxSS.str(std::string());*/
+
+		Client* c = new Particular(name, age, nif, address, zipCode, 1, 12, 2015);
+		company.addClient(c);
 	}
+
+	clientsFile.close();
 
 }
 
-void importInfo(){
+void importInfo(MovingCompany& company){
 	//importCountries();
-	importClients();
+	importClients(company);
 	//import
 }
 
@@ -50,12 +103,13 @@ void terminateProgram(){
 int main() {
 	MovingCompany company("EletroMoving, S.A.", "Great company with moving.", 10, 9, 1995);
 
-	Company* c1 = new Company("Eletrodomesticos Flavienses", 154654896, "Parque Industrial de Chaves, lote 75, II", "4400-589", 25,5,2016);
-	company.addClient(c1);
-
-	//importInfo();
+	importInfo(company);
 
 	//run(company);
+
+	for(unsigned int i = 0; i < company.getClients().size(); i++){
+		std::cout << *company.getClients()[i];
+	}
 
 	terminateProgram();
 
